362f914a145217327d65f644a26f4186b012c974
Vinam Arora <vinam@posteo.de>
2023-10-04 17:59:35 -0400

    Cleanup

--- finetune-vis.js
+++ finetune-vis.js
@@ -1,42 +1,45 @@
-// create a data source to hold data
-const source = new Bokeh.ColumnDataSource({
-    data: { x: [], y: [] }
-});
+window.addEventListener('load', finetune_vis);
 
-// make a plot with some tools
-const plot = Bokeh.Plotting.figure({
-    title: 'Example of random data',
-    height: 300,
-    width: 300
-});
+function finetune_vis() {
 
-// add a line with data from the source
-plot.line({ field: "x" }, { field: "y" }, {
-    source: source,
-    line_width: 2
-});
+    // create a data source to hold data
+    const source = new Bokeh.ColumnDataSource({
+        data: { x: [], y: [] }
+    });
 
-plot.toolbar.logo = null
-plot.toolbar_location = null
+    // make a plot with some tools
+    const plot = Bokeh.Plotting.figure({
+        title: 'Example of random data',
+        height: 300,
+        width: 300
+    });
 
-// show the plot, appending it to the end of the current section
-Bokeh.Plotting.show(plot, "#finetune-vis-plot1");
+    // add a line with data from the source
+    plot.line({ field: "x" }, { field: "y" }, {
+        source: source,
+        line_width: 2
+    });
 
-function addPoint() {
-    // add data --- all fields must be the same length.
-    source.data.x.push(Math.random())
-    source.data.y.push(Math.random())
+    plot.toolbar.logo = null
+    plot.toolbar_location = null
 
-    // update the data source with local changes
-    source.change.emit()
+    // show the plot, appending it to the end of the current section
+    Bokeh.Plotting.show(plot, "#finetune-vis-hand-vel-plot");
+
+    function addPoint() {
+        // add data --- all fields must be the same length.
+        source.data.x.push(Math.random())
+        source.data.y.push(Math.random())
+
+        // update the data source with local changes
+        source.change.emit()
+    }
+
+    const container = document.getElementById("finetune-vis-container");
+    const addDataButton = document.getElementById("finetune-vis-button");
+    addDataButton.addEventListener("click", addPoint);
+
+    addPoint();
+    addPoint();
 }
-
-const container = document.getElementById("finetune-visual");
-const addDataButton = document.createElement("Button");
-addDataButton.appendChild(document.createTextNode("Some data."));
-container.appendChild(addDataButton);
-addDataButton.addEventListener("click", addPoint);
-
-addPoint();
-addPoint();
 
